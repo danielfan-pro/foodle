5a79a993494ddcd94c45e33ea8bd943a76586603
Daniel Fan <xuhuafan@gmail.com>
2022-11-14 12:49:05 -0500

    aws upload, admin user and profile pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, uniqueness: true
+
+  mount_uploader :profile_photo, ProfilePhotoUploader
+        
 end
