956dd7691cb24ffab954b9dcdf3cee3540173f84
Daniel Fan <xuhuafan@gmail.com>
2022-11-19 23:21:06 -0500

    added footer partial and updated dark mode toggle

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -6,13 +6,22 @@ import RestaurantShowContainer from "./RestaurantShowContainer"
 
 export const App = () => {
   const [theme, setTheme] = useState("light")
+  const [moonStatus, setMoonStatus] = useState("show")
+  const [sunStatus, setSunStatus] = useState("hide")
+
   const toggleTheme = () => {
     if (theme === "light") {
       setTheme("dark")
+      setMoonStatus("hide")
+      setSunStatus("show")
     } else {
       setTheme("light")
+      setMoonStatus("show")
+      setSunStatus("hide")
     }
   }
+
+
 
   useEffect(() => {
     document.body.className = theme
@@ -20,7 +29,8 @@ export const App = () => {
 
   return (
     <div className={`App ${theme}`}>
-      <i className="fa-solid fa-sun dark-mode-toggle" onClick={toggleTheme}></i>
+      <i className={`fa-solid fa-moon dark-mode-toggle ${moonStatus}`} onClick={toggleTheme}></i>
+      <i className={`fa-solid fa-sun dark-mode-toggle ${sunStatus}`} onClick={toggleTheme}></i>
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={HomePage} />
